b33ba85daf4de82337f6cc58fcb5131a30c7c41b
Quentin <quentin1998-007@hotmail.fr>
2021-05-11 10:54:39 +0200

    Ajout du script de création de la base de donnée

---
+++ utils/bdd/bdd_creation.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS get_my_spot;
+USE get_my_spot;
+
+DROP TABLE IF EXISTS COLLECT;
+DROP TABLE IF EXISTS USER;
+
+CREATE TABLE USER
+(
+    pseudo VARCHAR(50)  PRIMARY KEY,
+    password VARCHAR(100),
+    image TEXT
+);
+
+CREATE TABLE COLLECT
+(
+    id_collect INTEGER  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    battery VARCHAR(10),
+    time DATE,
+    position_latitude DOUBLE,
+    position_longitude DOUBLE,
+    pressure VARCHAR(50),
+    brightness VARCHAR(50),
+    pseudo VARCHAR(50),
+    FOREIGN KEY(pseudo)  REFERENCES USER(pseudo)
+);
